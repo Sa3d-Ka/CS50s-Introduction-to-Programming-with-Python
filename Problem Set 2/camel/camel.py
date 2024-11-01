9ad99df914b055e7f59d832651771f368e3dd880
Saad Kanani <saad.kanani.off@gmail.com>
2024-11-01 01:45:39 +0100

    Delete Problem Set 2 directory

--- Problem Set 2/camel/camel.py
+++
@@ -1,9 +0,0 @@
-camel_case = input("camelCase: ")
-
-print("snake_case: ", end="")
-
-for i in camel_case:
-    if i.isupper():
-        print("_" + i.lower(), end="")
-    else:
-        print(i, end="")
