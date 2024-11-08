0a8c45024bf3e7b32d9dade5efaef1c4b048cb69
Saad Kanani <saad.kanani.off@gmail.com>
2024-11-08 22:26:46 +0100

    fuel

---
+++ Problem Set 3/fuel/fuel.py
@@ -0,0 +1,19 @@
+while True:
+    fuel = input("Fraction: ")    
+    try:
+        x, y = fuel.split("/")
+        new_x = int(x)
+        new_y = int(y)
+        f = new_x / new_y
+        if f <= 1:
+            break
+    except (ValueError, ZeroDivisionError):
+        pass
+        
+p = f*100
+if p == 100:
+    print("F")
+elif p <= 1:
+    print("E")
+else:
+    print(f"{p}%")
