e5b4798b44adef1755bfd6f8f3a5485143b4d193
Saad Kanani <saad.kanani.off@gmail.com>
2024-11-10 15:34:53 +0100

    taqueria

---
+++ Problem Set 3/taqueria/taqueria.py
@@ -0,0 +1,22 @@
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+S = 0
+while True:
+    try:
+        items = input("Item: ").title()  # Convert input to title case to match menu keys
+        if items in menu:
+            S += menu[items]
+            print(f"${S:.2f}")
+    except EOFError:
+        print("\nExiting...")
+        break
