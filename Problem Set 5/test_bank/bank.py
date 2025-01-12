017ba741b2dd015280962bd56d87bed228306db8
Saad Kanani <saad.kanani.off@gmail.com>
2025-01-12 19:06:12 +0100

    .

--- Problem Set 5/test_bank/bank.py
+++ Problem Set 5/test_bank/bank.py
@@ -1,14 +1,14 @@
 def main():
     greeting = input("Greeting: ")
-    print(value(greeting))
+    print(f"${value(greeting)}")
 
 def value(greeting):
     if "hello" in greeting.lower().strip() :
-        return "$0"
+        return 0
     elif greeting.lower().strip().startswith('h'):
-        return "$20"
+        return 20
     else:
-        return "$100"
+        return 100
 
 
 if __name__ == "__main__":
--- Problem Set 5/test_bank/test_bank.py
+++ Problem Set 5/test_bank/test_bank.py
@@ -7,15 +7,18 @@ def main():
     
 
 def test_hello():
-    assert value("hello") == "$0"
-    assert value(" hello  ") == "$0"
+    assert value("hello") == 0
+    assert value(" hello  ") == 0
+    assert value("HELLO") == 0
+    assert value("HellO") == 0
 
 def test_greeting_starts_with_h():
-    assert value(" hi") == "$20"
-    assert value("How you doing?") == "$20"
+    assert value(" hi") == 20
+    assert value("How you doing?") == 20
+    assert value("HoW YOU DOING?") == 20
 
 def test_other_greeting():
-    assert value("What's happening?") == "$100"
+    assert value("What's happening?") == 100
 
 if __name__ == "__main__":
     main()
