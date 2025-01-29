5bd0297ee6b03bf5370cef0419fe79bbf7d2895a
Saad Kanani <saad.kanani.off@gmail.com>
2025-01-30 00:11:23 +0100

    watch

---
+++ Problem set 7/watch/watch.py
@@ -0,0 +1,19 @@
+import re
+
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    pattern = r'<iframe.*?src="https?://(?:www\.)?youtube\.com/embed/([\w-]+)".*?>'
+    match = re.search(pattern, s)
+    if match:
+        video_id = match.group(1)
+        return f"https://youtu.be/{video_id}"
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    main()
