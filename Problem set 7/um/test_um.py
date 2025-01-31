5048f40abebdc11866c4fdbfca09036a3d9cce95
Saad Kanani <saad.kanani.off@gmail.com>
2025-01-31 18:01:58 +0100

    um

---
+++ Problem set 7/um/test_um.py
@@ -0,0 +1,11 @@
+from um import count
+
+def main():
+    test1()
+
+
+def test1():
+    assert count("um") == 1
+    assert count("um?") == 1
+    assert count("Um, thanks for the album") == 1
+    assert count("Um, thanks, um...") == 2
---
+++ Problem set 7/um/um.py
@@ -0,0 +1,15 @@
+import re
+
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    pattern = r"[^.]um[^\w]"
+    x = re.findall(pattern, s, flags=re.IGNORECASE)
+    return len(x)
+
+
+if __name__ == "__main__":
+    main()
